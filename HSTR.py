3394d032824a658582eac07e3bffe14077f8d01e
umutsuluhan <suluhanumut@gmail.com>
2021-09-17 18:14:01 +0300

    Video input to model is completed

--- HSTR.py
+++ HSTR.py
@@ -12,7 +12,7 @@ class Model():
         self.flownet = IFNet()
         self.flownet.to(device)
         
-    def inference(self, imgs, training=False):
+    def inference(self, imgs, timestamps,training=False):
         
         # lfr_img0 = lr(t-1)
         # lfr_img1 = lr(t)
@@ -20,16 +20,19 @@ class Model():
         # hfr_img0 = hr(t-1)
         # hfr_img1 = hr(t+1)
         
-        lfr_img0 = imgs[:, :3]
-        lfr_img1 = imgs[:, 3:6]
-        lfr_img2 = imgs[:, 6:9]
-        hfr_img0 = imgs[:, 9:12]
-        hfr_img1 = imgs[:, 12:15]
+        lfr_img0 = imgs[:, :, :3]
+        lfr_img1 = imgs[:, :, 3:6]
+        lfr_img2 = imgs[:, :, 6:9]
+        hfr_img0 = imgs[:, :, 9:12]
+        hfr_img1 = imgs[:, :, 12:15]
         
-        hfr_prev_to_next_flow, _ = self.flownet(torch.cat((hfr_img0, hfr_img1),1))
-        hfr_next_to_prev_flow, _ = self.flownet(torch.cat((hfr_img1, hfr_img0),1))
-        lfr_prev_to_mid_flow,  _ = self.flownet(torch.cat((lfr_img0, lfr_img1),1))
-        lfr_mid_to_next_flow,  _ = self.flownet(torch.cat((lfr_img1, lfr_img2),1))
+        for entry in timestamps:
+            print(entry)
+        
+        # hfr_prev_to_next_flow, _ = self.flownet(torch.cat((hfr_img0, hfr_img1),1))
+        # hfr_next_to_prev_flow, _ = self.flownet(torch.cat((hfr_img1, hfr_img0),1))
+        # lfr_prev_to_mid_flow,  _ = self.flownet(torch.cat((lfr_img0, lfr_img1),1))
+        # lfr_mid_to_next_flow,  _ = self.flownet(torch.cat((lfr_img1, lfr_img2),1))
         
         # print(hfr_prev_to_next_flow.shape)
         # print(hfr_next_to_prev_flow.shape)
--- test_video.py
+++ test_video.py
@@ -1,55 +1,69 @@
 import cv2
+import numpy as np
+import torch
 import matplotlib.pyplot as plt
+from HSTR import Model
+import time
 
 if __name__ == '__main__':
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")    
 
     lfr_video = "videos/demo.mp4"
     hfr_video = "videos/demo_2X_50fps.mp4"
 
+    hstr_model = Model()
+
+    start_time = time.time()
+
+    #Getting fps and total frame number of low frame rate video
     lfr_video_capture = cv2.VideoCapture(lfr_video)
-    lfr_fps = lfr_video_capture.get(cv2.CAP_PROP_FPS)
+    lfr_fps = lfr_video_capture.get(cv2.CAP_PROP_FPS)   
     lfr_tot_frame = lfr_video_capture.get(cv2.CAP_PROP_FRAME_COUNT)
     
+    #Getting timestamp of each frames
     lfr_timestamps = [lfr_video_capture.get(cv2.CAP_PROP_POS_MSEC)]
-    lfr_proc_timestamps = [0.0]
+    lfr_proc_timestamps = []
+    lfr_frames = []
     while(lfr_video_capture.isOpened()):
         frame_exists, curr_frame = lfr_video_capture.read()
         if(frame_exists):
-            plt.imshow(curr_frame)
-        #     lfr_timestamps.append(lfr_video_capture.get(cv2.CAP_PROP_POS_MSEC))
-        #     frame_and_timestamp = (curr_frame, lfr_timestamps[-1] + 1000/lfr_fps)
-        #     lfr_proc_timestamps.append(curr_frame : lfr_timestamps[-1] + 1000/lfr_fps)
+            lfr_timestamps.append(lfr_video_capture.get(cv2.CAP_PROP_POS_MSEC))
+            lfr_proc_timestamps.append(lfr_timestamps[-1] + 1000/lfr_fps)
+            lfr_frames.append(curr_frame)
         else:
              break
             
     lfr_video_capture.release()
     
+    #Getting fps and total frame number of high frame rate video
     hfr_video_capture = cv2.VideoCapture(hfr_video)
     hfr_fps = hfr_video_capture.get(cv2.CAP_PROP_FPS)
     hfr_tot_frame = hfr_video_capture.get(cv2.CAP_PROP_FRAME_COUNT)
-    hfr_timestamps = [hfr_video_capture.get(cv2.CAP_PROP_POS_MSEC)]
     
-    
+    #Getting timestamp of each frames
     hfr_timestamps = [hfr_video_capture.get(cv2.CAP_PROP_POS_MSEC)]
-    hfr_proc_timestamps = [0.0]
+    hfr_proc_timestamps = []
+    hfr_frames = []
     while(hfr_video_capture.isOpened()):
         frame_exists, curr_frame = hfr_video_capture.read()
         if(frame_exists):
             hfr_timestamps.append(hfr_video_capture.get(cv2.CAP_PROP_POS_MSEC))
             hfr_proc_timestamps.append(hfr_timestamps[-1] + 1000/lfr_fps)
+            hfr_frames.append(curr_frame)
         else:
             break
     
     hfr_video_capture.release()
+   
+    finish_time = time.time()
 
+    imgs = np.concatenate((lfr_frames[0], lfr_frames[1], hfr_frames[0], hfr_frames[1], hfr_frames[2]), 2)
+    timestamps = []
+    timestamps.append(lfr_proc_timestamps[0])
+    timestamps.append(lfr_proc_timestamps[1])
+    timestamps.append(hfr_proc_timestamps[0])
+    timestamps.append(hfr_proc_timestamps[1])
+    timestamps.append(hfr_proc_timestamps[2])
     
-    print(len(lfr_proc_timestamps))
-    print(len(hfr_timestamps))
-    print(lfr_proc_timestamps[0])
-
-    # videogen = skvideo.io.vreader(args.video)
-    # lastframe = next(videogen)
-    # fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-    # video_path_wo_ext, ext = os.path.splitext(args.video)
-    # print('{}.{}, {} frames in total, {}FPS to {}FPS'.format(
-    # video_path_wo_ext, args.ext, tot_frame, fps, args.fps))
+    hstr_model.inference(imgs, timestamps)
