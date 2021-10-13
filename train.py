5fe85aa79de17196fb2a8cf4b73451f8dad3341f
umutsuluhan <suluhanumut@gmail.com>
2021-10-13 13:05:56 +0300

    Commit test

--- train.py
+++ train.py
@@ -127,7 +127,7 @@ def train(model, data_root_p):
             end = time.time()
 
         
-            if(trainIndex % 500 == 0):
+            if(trainIndex % 2500 == 0):
 
                 print("Validating, Train Index: " + str(trainIndex))
                 logging.info("Validating, Train Index: " + str(trainIndex))
